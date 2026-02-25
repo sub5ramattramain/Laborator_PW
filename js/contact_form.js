dcb226b44b0c97f3ab0e6d2289ef27550c2d906a
sub5ramattramain <fpoparosu@gmail.com>
2026-02-26 00:27:21 +0200

    feat: adauga salut personalizat pe baza orei

--- js/contact_form.js
+++ js/contact_form.js
@@ -1,3 +1,21 @@
+const headerParagraf = document.querySelector('header p');
+
+const oraCurenta = new Date().getHours();
+
+let mesajSalut = "";
+
+if (oraCurenta >= 6 && oraCurenta < 12) {
+    mesajSalut = "neata! welcome to my page vro"; 
+} else if (oraCurenta >= 12 && oraCurenta < 18) {
+    mesajSalut = "buna ziua vro! welcome to my page";       
+} else {
+    mesajSalut = "buna seara vro! welcome to my page";      
+}
+
+if (headerParagraf) {
+    headerParagraf.textContent = mesajSalut;
+}
+
 function submitForm() {
     const nume = document.getElementById("name").value;
     const email = document.getElementById("email").value;
