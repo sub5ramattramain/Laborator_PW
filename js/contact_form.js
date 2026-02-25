0a675c6baedb7a66211c74de5754a17701de9073
sub5ramattramain <fpoparosu@gmail.com>
2026-02-26 00:09:33 +0200

    Finished Laborator 1

--- js/contact_form.js
+++ js/contact_form.js
@@ -1,4 +1,11 @@
-function submitForm()
-{
-    const nume=document.getElementById("name").va
+function submitForm() {
+    const nume = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const mesaj = document.getElementById("message").value;
+
+    console.log("Numele introdus:", nume);
+    console.log("Emailul introdus:", email);
+    console.log("Mesajul introdus:", mesaj);
+
+    console.warn("Goodbye World!");
 }
