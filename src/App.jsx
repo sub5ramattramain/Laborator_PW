ec5efd0a3bc8108a80ca17e1a7c00200779ba0a0
sub5ramattramain <fpoparosu@gmail.com>
2026-03-18 15:35:07 +0200

    feat: added count button

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,23 @@
 import Card from './Card';
+import { useState } from 'react';
+
 const projects = [
 { title: "Proiect 1", description: "Pagina personala" },
 { title: "Proiect 2", description: "Calculator buget" },
 { title: "Proiect 3", description: "Dashboard React" },
 ];
+
 function App() {
+  const [count, setCount] = useState(0);
 return (
 <div>
 <h1>Dashboard</h1>
 {projects.map(function(item, index) {
 return <Card key={index} title={item.title} description={item.description} />;
 })}
-<p>Numele vostru </p>
+<p>Numele vostru </p><br></br>
+<p>Ai apasat de {count} ori</p>
+<button onClick={() => setCount(count + 1)}>Click</button>
 </div>
 );
 }
