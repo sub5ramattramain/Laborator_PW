a2cc358b20101c8ffeee9737de0df9da4e874351
sub5ramattramain <fpoparosu@gmail.com>
2026-05-13 15:50:05 +0300

    terminat lab 7 exercitiul 3

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Home from './pages/Home';
 import Projects from './pages/Projects';
 import Contact from './pages/Contact';
 import Navbar from './Navbar';
+import './App.css';
 function App() {
   return (
     <BrowserRouter>
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,11 +1,11 @@
-import { Link } from 'react-router';
+import { NavLink } from 'react-router';
 
 function Navbar() {
     return (
         <nav>
-            <Link to="/">Home</Link>
-            <Link to="/projects">Proiecte</Link>
-            <Link to="/contact">Contact</Link>
+            <NavLink to="/">Home</NavLink>
+            <NavLink to="/projects">Proiecte</NavLink>
+            <NavLink to="/contact">Contact</NavLink>
         </nav>
     );
 }
