d0bf2d634a0b85ab5baa27bdd8692c186e81a392
sub5ramattramain <fpoparosu@gmail.com>
2026-05-17 17:20:57 +0300

    :terminat lab 8 exercitiul 4

--- server/index.js
+++ server/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const PORT = 3000;
+
+app.use(cors());
+app.use(express.json());
 // Prima ruta: raspunde la GET /
 app.get('/', function (req, res) {
     res.json({ message: 'Serverul functioneaza!' });
@@ -40,6 +44,18 @@ app.get('/api/stats', function(req, res) {
     finalizate: finalizate,
     inLucru: inLucru
   });
+});
+
+// POST /api/projects - adauga un proiect nou
+app.post('/api/projects', function(req, res) {
+const newProject = {
+id: projects.length + 1,
+title: req.body.title,
+tech: req.body.tech,
+done: req.body.done || false,
+};
+projects.push(newProject);
+res.status(201).json(newProject);
 });
 
 app.listen(PORT, function () {
