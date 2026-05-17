6fbf60dfd9f7a8d8f0d5eaae6a42f4f161dea5a7
sub5ramattramain <fpoparosu@gmail.com>
2026-05-17 18:52:11 +0300

    terminat lab 10 exercitiile 1-2

--- src/ProjectList.jsx
+++ src/ProjectList.jsx
@@ -7,12 +7,12 @@ function ProjectList() {
   const [search, setSearch] = useState('');
 
   useEffect(function() {
-    fetch('/data/projects.json')
+    fetch('http://localhost:3000/api/projects')
       .then(function(response) {
         return response.json();
       })
       .then(function(data) {
-        setProjects(data.projects);
+        setProjects(data);
         setLoading(false);
       });
   }, []);
