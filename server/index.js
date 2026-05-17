0bfa5b3b176a3176f4f8fbe2f82d73be9d32e54a
sub5ramattramain <fpoparosu@gmail.com>
2026-05-17 16:59:18 +0300

    terminat lab 8 exercitiul 2

--- server/index.js
+++ server/index.js
@@ -6,6 +6,17 @@ app.get('/', function(req, res) {
 res.json({ message: 'Serverul functioneaza!' });
 });
 // Porneste serverul
+// Date (temporar in memorie, vom folosi MongoDB mai tarziu)
+const projects = [
+{ id: 1, title: "Pagina Personala", tech: "HTML, CSS", done: true },
+{ id: 2, title: "Calculator Buget", tech: "JS", done: true },
+{ id: 3, title: "Dashboard React", tech: "React", done: false },
+{ id: 4, title: "API Meteo", tech: "React, API", done: false },
+];
+// GET /api/projects - returneaza toate proiectele
+app.get('/api/projects', function(req, res) {
+res.json(projects);
+});
 app.listen(PORT, function() {
 console.log('Server pornit pe http://localhost:' + PORT);
 });
