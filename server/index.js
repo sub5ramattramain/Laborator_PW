79e722a8a3fda5bd043a59484abb0d05a13ab629
sub5ramattramain <fpoparosu@gmail.com>
2026-05-17 18:01:14 +0300

    terminat lab 9 exercitiul 1

--- server/index.js
+++ server/index.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/dashboard')
+.then(function() {
+console.log('Conectat la MongoDB!');
+})
+.catch(function(err) {
+console.error('Eroare conectare MongoDB:', err);
+});
 const PORT = 3000;
 
 app.use(cors());
